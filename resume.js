c07daa48a0959e6d74958d7ea6e076423ae63117
gollavilli ganesh kumar <84216653+ganesh2328@users.noreply.github.com>
2021-06-21 10:07:50 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,98 @@
+var obj = 
+[
+  {
+    "contact": {
+      " firstname ": "Gollavilli",
+      "middle_name": "Ganesh",
+      "last_name": "Kumar",
+      "city ": "Visakhaptnam",
+      "State": "AndhraPradesh",
+      "Email": "ganeshgollavilli@gmail.com"
+    }
+  },
+  {
+    "CV Highlights": {
+      "1": [
+        "Learnt basic javascript HTML CSS on Udemy"
+      ],
+      "2": [
+        "An independent,dedicated and efficient person.These attributes proven through series of courses i have taken or I am taking independently through online platforms."
+      ],
+      "3": [
+        "Good communication skills,Presentation skills.,Attitude towords leadership,auttorsition and delegation,conflict resolution negotiation and a good team worker."
+      ]
+    }
+  },
+  {
+    "EDUCATIONAL QUALIFICATIONS": [
+      {
+        "Qualification": [
+          "S.S.C",
+          "Intermediate",
+          "B.Sc"
+        ]
+      },
+      {
+        "Board/University": [
+          "AndhraPradesh State Board",
+          "AndhraPradesh Board",
+          "Andhra University"
+        ]
+      },
+      {
+        "Percentage/Grades": [
+          "9.3",
+          "84%",
+          "84.33%"
+        ]
+      },
+      {
+        "Year": [
+          "2012",
+          "2014",
+          "84.33%"
+        ]
+      }
+    ]
+  },
+  {
+    "Personal Information": {
+      " Interests ": "RoadTrips",
+      " Languages Known": [
+        "Telugu",
+        "English"
+      ],
+      "Hobbies": [
+        "cooking",
+        "playing cricket",
+        "watching movies",
+        "listening music"
+      ]
+    }
+  },
+  {
+    "Other Information": [
+      {
+        " Expected Salary": [
+          "As per the company standards"
+        ],
+        " Area of Interests ": [
+          "Software Development",
+          "Programming",
+          "App Development",
+          "Coding",
+          "Startups",
+          "Technical Support",
+          "Support Engineer",
+          "Joining Date : Intermediate"
+        ]
+      }
+    ]
+  },
+  {
+    "Declaration ": [
+      "I here by declare that the details furnished above are true and correct to the best of my knowledge and belief."
+    ]
+  }
+]
+console.log(obj);
